eb5675e0534e3209665c60a79a9b51d380edff23
doy00 <dhkm201@gmail.com>
2024-08-27 01:27:15 +0900

    NavBar만 담던 Header.jsx를 지우고 NavBar를 App.js에 직접 임포트

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,60 +1,23 @@
-// src/App.js 
-// import React from "react";
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Navbar from "./components/Navbar";
-// import Footer from "./components/Footer";
-// import Main from "./pages/Main";
-// import MyLike from "./pages/MyLike";
-// import MyPage from "./pages/MyPage";
-// import ShoppingCart from "./pages/ShoppingCart";
-// import SignIn from "./pages/SignIn";
-// import SignUp from "./pages/SignUp";
-// import 'bootstrap/dist/css/bootstrap.min.css'; // 부트스트랩 설치
-// import {PrivateRoute} from './components/PrivateRoute';
-// import './App.css';
-// import Category from "./components/Category";
+/* 
+src/App.jsx
+실제 렌더링 되는 App은 App.js임 주의
+*/
 
-
-
-
-// const App = () => {
-//   return (    
-//     <Router>
-//       < div className="main-container">
-//       <div className="landing-page">
-//         <Navbar />
-//         <Category />
-//         <Routes>
-//           <Route path="/" element={<Main />} />
-//           <Route path="/MyLike" element={<PrivateRoute element={<MyLike />} />} />
-//           <Route path="/MyPage" element={<PrivateRoute element={<MyPage />} />} />
-//           <Route path="/ShoppingCart" element={<PrivateRoute element={<ShoppingCart />} />}  />
-//           <Route path="/SignIn" element={<SignIn />} />
-//           <Route path="/SignUp" element={<SignUp />} /> 
-//         </Routes>
-//         <Footer />
-//         </div>
-//       </div>
-// //     </Router>
-    
-//   );
-// };
-
-// export default App;
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Layout/Header';
+import NavBar from "./components/Layout/NavBar";
 import Footer from "./components/Footer";
 import MainPage from "./pages/MainPage";
+import {PrivateRoute} from './components/PrivateRoute';
 import MyLike from "./pages/MyLike";
 import MyPage from "./pages/MyPage";
 import ShoppingCart from "./pages/ShoppingCart";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import ProductList from "./components/ProductList";
+
 import 'bootstrap/dist/css/bootstrap.min.css'; // 부트스트랩 설치
-import {PrivateRoute} from './components/PrivateRoute';
 import './App.css';
-import Category from "./components/Category";
 
 
 
@@ -62,10 +25,9 @@ import Category from "./components/Category";
 const App = () => {
   return (    
     <Router>
-      < div className="main-container">
+      <div className="main-container">
       <div className="landing-page">
-        <Header />
-        {/* <Category /> */}
+        <NavBar />
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/MyLike" element={<PrivateRoute element={<MyLike />} />} />
@@ -73,6 +35,9 @@ const App = () => {
           <Route path="/ShoppingCart" element={<PrivateRoute element={<ShoppingCart />} />}  />
           <Route path="/SignIn" element={<SignIn />} />
           <Route path="/SignUp" element={<SignUp />} /> 
+          <Route path="/ProductList" element={<ProductList />} />
+          <Route path="/ProductList/:productId" element={<ProductList />} />
+
         </Routes>
         <Footer />
         </div>
--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -1,22 +1,23 @@
 /* 
-dhkim
+src/App.jsx
+실제 렌더링 되는 App은 App.js임 주의
 */
 
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Layout/Header';
+import NavBar from "./components/Layout/NavBar";
 import Footer from "./components/Footer";
 import MainPage from "./pages/MainPage";
+import {PrivateRoute} from './components/PrivateRoute';
 import MyLike from "./pages/MyLike";
 import MyPage from "./pages/MyPage";
 import ShoppingCart from "./pages/ShoppingCart";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
-import 'bootstrap/dist/css/bootstrap.min.css'; // 부트스트랩 설치
-import {PrivateRoute} from './components/PrivateRoute';
-import './App.css';
-import Category from "./components/Category";
 import ProductList from "./components/ProductList";
+
+import 'bootstrap/dist/css/bootstrap.min.css'; // 부트스트랩 설치
+import './App.css';
 
 
 
@@ -26,8 +27,7 @@ const App = () => {
     <Router>
       < div className="main-container">
       <div className="landing-page">
-        <Header />
-        {/* <Category /> */}
+        <NavBar />
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/MyLike" element={<PrivateRoute element={<MyLike />} />} />
