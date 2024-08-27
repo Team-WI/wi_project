2fd6e164ec37591af8d3c83980d2e6d67a6e5f8e
dhkim <dhkm201@gmail.com>
2024-08-27 11:00:07 +0900

    네비바 css 수정

--- reactapp/src/components/Layout/NavBar.jsx
+++ reactapp/src/components/Layout/NavBar.jsx
@@ -10,7 +10,7 @@ import { Container, Nav, Navbar, Form, Button, InputGroup } from 'react-bootstra
 import '../Components.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
-import '../../assets/styles/NavBar.css'; 
+import './NavBar.css'; 
 
 const NavBar = () => {
   const [expanded, setExpanded] = useState(false);
@@ -19,7 +19,7 @@ const NavBar = () => {
 
   return (
     <div>
-    <Navbar bg="white" variant="dark" expand="lg" expanded={expanded} sticky="top">
+    <Navbar bg="white" variant="light" expand="lg" expanded={expanded} sticky="top">
       <Container fluid>
         <Navbar.Brand as={Link} to="/">WI Mall</Navbar.Brand>
         <Navbar.Toggle 
@@ -60,95 +60,8 @@ const NavBar = () => {
         </Navbar.Collapse>
       </Container>
     </Navbar>
-    {/* <Category /> */}
     </div>
   );
 };
 
 export default NavBar;
-
-// const Navbar = () => {
-// if (response) {
-//   return (
-//     <nav className="navbar">
-//     <div className="logo">
-//       <Link to="/">WI Mall</Link>
-//     </div>
-//     <ul className="nav-links">
-//       <li>
-//         <Link to="/MyLike">MyLike</Link>
-//       </li>
-//       <li>
-//         <Link to="/MyPage">MyPage</Link>
-//       </li>
-//       <li>
-//         <Link to="/ShoppingCart">ShoppingCart</Link>
-//       </li>
-//       <li>
-//       <Link to="/SignIn">LogIn</Link>
-//     </li>
-//     </ul>
-//   </nav>
-//   ); 
- 
-// } else {
-//   return (
-//     <nav className="navbar">
-//     <div className="logo">
-//       <Link to="/">WI Mall</Link>
-//     </div>
-//     <ul className="nav-links">
-//       <li>
-//         <Link to="/MyLike">MyLike</Link>
-//       </li>
-//       <li>
-//         <Link to="/MyPage">MyPage</Link>
-//       </li>
-//       <li>
-//         <Link to="/ShoppingCart">ShoppingCart</Link>
-//       </li>
-//       <li>
-//       <Link to="/SignIn">LogOut</Link>
-//     </li>
-//     </ul>
-//   </nav>
-//   ); 
-// }
-  
-// };
-
-// const Navbar = ({ isLogin }) => {
-
-//   return (
-//     <nav className="navbar">
-//     <div className="logo">
-//       <Link to="/">WI Mall</Link>
-//     </div>
-//     <ul className="nav-links">
-//       <li>
-//         <Link to="/MyLike">MyLike</Link>
-//       </li>
-//       <li>
-//         <Link to="/MyPage">MyPage</Link>
-//       </li>
-//       <li>
-//         <Link to="/ShoppingCart">ShoppingCart</Link>
-//       </li>
-//       <li>
-//       <Link to="/SignIn">LogIn</Link>
-//     </li>
-//     </ul>
-//   </nav>
-//   );
-// };
-
-// export default Navbar;
-
-
-
-// 테스트: 세션스토리지에 토큰이 남아있으면 로그인 된 상태, 상단 내비바 LogIn태그가 -> LogOut으로 변환
-//{isLogin  ? (<Link to onClick={logoutToken}>로그아웃</Link>) : ( <Link onClick={() => navigate("/SignIn")}>로그인</Link>)}
-//{isLogin ? (<Link to="/SignIn">로그아웃</Link>) : (<Link to="/SignIn">로그인</Link>)}
-
-
-
